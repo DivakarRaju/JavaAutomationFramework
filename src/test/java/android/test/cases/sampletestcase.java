bdd3b23de3103d0cdad2ff27339f783ab3297894
Divakar Raju <divakarrj875@gmail.com>
2018-06-07 23:22:06 +0530

    added a web test case and executed

--- src/test/java/android/test/cases/sampletestcase.java
+++ src/test/java/android/test/cases/sampletestcase.java
@@ -2,6 +2,11 @@ package android.test.cases;
 
 import java.net.MalformedURLException;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumDriver;
@@ -10,13 +15,34 @@ import support.*;
 
 public class sampletestcase {
 	
+	BrowserUtilities brutils = new BrowserUtilities();
 	AppiumDriver<MobileElement> appdriver;
+	WebDriver driver;
+	
+//	@Test
+//	public void apptestmethod() throws MalformedURLException{
+//		
+//		appdriver= AppUtilities.appiumdriver_initialize();
+//		appdriver.quit();
+//	}
+//	
 	
 	@Test
-	public void apptestmethod() throws MalformedURLException{
+	public void webtestmethod() throws Exception{
 		
-		appdriver= AppUtilities.appiumdriver_initialize();
-		appdriver.quit();
+		driver = BrowserUtilities.webdriver_initialize();
+		driver.navigate().to("https://www.wikipedia.org/");
+		System.out.println("navigating to wikipedia url");
+		
+		driver.manage().window().maximize();
+		
+		BrowserUtilities.webdriver_wait(driver).until(ExpectedConditions.visibilityOfElementLocated(By.id("js-link-box-en")));
+		System.out.println("English element displayed");
+		driver.findElement(By.id("js-link-box-en")).click();
+		System.out.println("successfully clicked on english language");
+		
+		Assert.assertTrue(driver.findElement(By.cssSelector("input#searchInput")).isDisplayed());
+		System.out.println("successfuly verified the search field");	
+		BrowserUtilities.webdriver_quit(driver);
 	}
 }
-;
--- src/test/java/support/BrowserUtilities.java
+++ src/test/java/support/BrowserUtilities.java
@@ -3,6 +3,7 @@ package support;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BrowserUtilities {
 	
@@ -11,13 +12,27 @@ public class BrowserUtilities {
 	public static WebDriver webdriver_initialize()
 	{
 		try{
-			System.setProperty("webdriver.chrome.driver","C:\\Ruby193\\bin\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver","F:\\Programming\\Divakar\\Testing\\webdrivers\\chromedriver.exe");
 			webdriver = new ChromeDriver();
+			System.out.println("started the web driver");
 			
 		}catch (Exception e){
 			e.printStackTrace();
 		}
 		return webdriver;
 	}
+	
+	public static void webdriver_quit(WebDriver driver)
+	{
+		System.out.println("quiting web driver");
+		driver.quit();
+	}
+	
+	public static WebDriverWait webdriver_wait(WebDriver driver)
+	{
+		WebDriverWait wait= new WebDriverWait(driver, 20);
+		return wait;
+	}
+	
 	
 }
